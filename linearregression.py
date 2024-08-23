8dc9c39e748c46f8ea7fdf490f8b717f46468507
Kartekeya-Sharma <110166739+Kartekeya-Sharma@users.noreply.github.com>
2024-08-23 23:22:35 +0530

    Update linearregression.py

--- linearregression.py
+++ linearregression.py
@@ -2,7 +2,7 @@
 import sklearn
 from sklearn import linear_model
 height=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
-weight=[  16, 25 , 36, 49, 64, 81, 100]
+weight=[  8, 10 , 12, 14, 16, 18, 20]
 reg=linear_model.LinearRegression()
 reg.fit(height,weight)
 X_height=[[12.0]]
